5e579fe71f8f2746aad4d2a06612019a6aa4a7f1
wankhede04 <wankhedevijay04@gmail.com>
2023-10-05 19:49:34 +0530

    feat: add transaction service

--- app/transactions/transactions.service.js
+++ app/transactions/transactions.service.js
@@ -1,35 +1,102 @@
-const { default: axios } = require('axios');
-const https = require('axios');
+const axios = require('axios');
+const Transaction = require('./dto/transaction.dto')
 
 
-function fetchWithdrawls() {
+module.exports = { fetchTransactions, queryTransaction, sumOfTransactions }
+
+
+async function getTimestamp() {
+    const lastRecord = await Transaction.findOne({}, null, { sort: { timestamp: -1 } })
+    return lastRecord != undefined ? lastRecord.timestamp.toString() : "1695686400"
+}
+
+async function generateQuery() {
+
+    const timestamp = await getTimestamp()
+
+    console.log("fetching since  - ", timestamp);
+
     const query = JSON.stringify({
         query: `
     {
-        withdraws{
+        withdraws(where: {timestamp_gt: "${timestamp}"}){
         from
         to
         id
         amountUSD
+        timestamp
         }
-    }
-`,
-    });
+    }`});
 
-    const headers = {
-        "content-type": "application/json"
+
+    return query
+}
+
+async function fetchTransactions() {
+    while (true) {
+        const query = await generateQuery()
+
+        const headers = {
+            "content-type": "application/json"
+        }
+
+        try {
+            const response = await axios({
+                url: process.env.GRAPH_ADDRESS,
+                method: 'post',
+                headers: headers,
+                data: query
+            })
+            if (response.data.data.withdraws.length == 0) {
+                console.log("db in sync with graph!");
+                break
+            }
+
+            await saveTransactions(response.data)
+
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 
-    const options = {
-        "method": "POST",
-        "headers": headers,
-        "body": query
+}
+
+async function saveTransactions(data) {
+    try {
+
+        await Transaction.create(...data.data.withdraws)
+        console.log("inserted - ", data.data.withdraws.length, " records!");
+    } catch (error) {
+        console.log(console.error());
+    }
+}
+
+async function queryTransaction(limit, skip, wallet_address) {
+    return await Transaction.find(wallet_address ? { to: wallet_address } : {}).skip(skip || 0).limit(limit || 5)
+}
+
+async function sumOfTransactions(wallet_address) {
+    let agr_pipeline =
+        [
+            {
+                $group: {
+                    _id: "$to",
+                    sum: {
+                        $sum:
+                            "$amountUSD"
+                    }
+                }
+            }
+        ]
+        console.log(wallet_address);
+    if (wallet_address) {
+        agr_pipeline.push({
+            $match: { _id: wallet_address }
+        })
     }
 
-    fetch(process.env.GRAPH_ADDRESS, options).then((data) => {
-        console.log(data);
-    }, (err) => {
-        console.log(err);
-    })
+
+    return await Transaction.aggregate(agr_pipeline)
 
 }
