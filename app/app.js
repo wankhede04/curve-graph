184494d3f2b9536f8011d15986eb1e633de65749
wankhede04 <wankhedevijay04@gmail.com>
2023-10-05 19:49:34 +0530

    fix: change cron to every 3 minute

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ app.use('/v1', require('./transactions/transaction.controller'));
 
 const port = process.env.PORT
 
-cron.schedule('* * * * *', () => {
+cron.schedule('*/3 * * * *', () => {
     console.log('started fetching data!');
 fetchTransactions() 
 });
